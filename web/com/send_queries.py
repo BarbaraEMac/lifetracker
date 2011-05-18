e7dd49c75fbc14e17aaa5609423569f0c28bebd4
gareth <gareth@PaloAlto.(none)>
2011-05-18 15:42:45 -0400

    Two things:   1) Don't update the lastSentAt on a query until we know its been sent     successfully   1) Don't send queries at nighttime.

--- web/com/send_queries.py
+++ web/com/send_queries.py
@@ -6,6 +6,7 @@ from datetime import datetime
 
 from model import User, Query, DataPoint
 from sms import send_sms
+from analytics.multi_analytics import nearest_day
 
 from datetime import datetime
 
@@ -32,12 +33,27 @@ def send_query_by_sms(query):
   send_sms(query.user.phone, text)  
 
 def send_query(query):
+  if query.user.query_medium == 'sms':
+    send_query_by_sms(query)
+  else:
+    send_by_email(query)
+
   query.lastSentAt = datetime.now() # refresh the query
   query.put() # commit it
-  if query.user.query_medium == 'sms':
-    return send_query_by_sms(query)
+
+def morning(timestamp):
+  return nearest_day(timestamp) + 10*60*60 # 10 am
+
+def nighttime(timestamp):
+  return nearest_day(timestamp) + 23*60*60 + 59*60 # 23:59
+
+def is_daytime():
+  # if it is between 10 am and midnight
+  now = datetime.now()
+  if now > morning(now) and now < nighttime(now):
+    return True
   else:
-    return send_by_email(query)
+    return False
 
 # it will be a problem if this takes a long time
 class SendQueriesHandler(webapp.RequestHandler):
@@ -47,7 +63,7 @@ class SendQueriesHandler(webapp.RequestHandler):
     queries = Query.all().fetch(1000)
 
     for query in queries:
-      if query.is_stale():
+      if query.is_stale() && is_daytime(): # only send when people are wake
         print query.name + " is stale"
         send_query(query)
         return
