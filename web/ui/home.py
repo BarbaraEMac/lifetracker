7b86cd780080f7590b1b06b7f11d94c39f642549
gareth <gareth@PaloAlto.(none)>
2011-09-07 14:51:18 -0400

    Redirecting the homepage to /dashboard for now while we build a better homepage.

--- web/ui/home.py
+++ web/ui/home.py
@@ -1,8 +1,27 @@
 from google.appengine.ext import webapp
+from google.appengine.api import users
+
+from model import User
+
+from constants import whitelist
 
 class HomeHandler(webapp.RequestHandler):
   def get(self):
-      html_file = open("ui/html/home.html")
-      html = html_file.read()
+    google_user = users.get_current_user()
+    if google_user == None:
+      self.redirect(users.create_login_url(self.request.uri))
+    elif not google_user.email() in whitelist:
+      self.redirect(users.create_logout_url(self.request.uri))
+    else:
+      user = User.get_by_google_user(google_user)
+      if user == None:
+        user = User(google_user = google_user, 
+          first_name='', 
+          last_name='', 
+          email=google_user.email()
+        )
 
-      self.response.out.write(html)
+        user.put() 
+
+      self.redirect('/dashboard');
+      
