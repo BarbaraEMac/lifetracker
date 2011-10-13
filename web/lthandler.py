a69d9dda157752c9396001fbe326db2069d4d62c
gareth <gareth@PaloAlto.(none)>
2011-10-12 22:20:10 -0400

    Missed lthandler.py in the login refactor. Oooops.

---
+++ web/lthandler.py
@@ -0,0 +1,31 @@
+from google.appengine.ext import webapp
+from google.appengine.api import users
+
+from model import User, Query, DataPoint, TemplateMetric, ActionLog
+
+from analytics.analytics import overview
+from constants import whitelist, admins
+import logging
+
+class LTHandler(webapp.RequestHandler):
+  def get_user(self):
+    user = None
+
+    google_user = users.get_current_user()
+    if google_user == None:
+      self.redirect(users.create_login_url(self.request.uri))
+    elif not google_user.email() in whitelist:
+      self.redirect(users.create_logout_url(self.request.uri))
+    else:
+      user = User.get_by_google_user(google_user)
+      if user == None:
+        user = User(google_user = google_user, 
+          first_name='', 
+          last_name='', 
+          email=google_user.email()
+        )
+
+        user.put() 
+
+    return user
+
