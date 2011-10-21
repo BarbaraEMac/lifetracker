4baddabd78b3ffa8f069e6816066b4f4f3ae03c8
gareth <gareth@PaloAlto.(none)>
2011-10-20 22:43:40 -0400

    Memcache on the data page!
    Threw in a bit of a refactor of the data page too.

--- web/ui/manage_data.py
+++ web/ui/manage_data.py
@@ -1,9 +1,12 @@
 from google.appengine.ext import webapp
 from google.appengine.api import users
+from google.appengine.api import memcache
 
 from model import User, Query, DataPoint
 from lthandler import LoggedInPageHandler
 from constants import whitelist
+
+from datetime import datetime
 
 class ManageDataHandler(LoggedInPageHandler):
   def get(self):
@@ -36,23 +39,46 @@ class ManageDataHandler(LoggedInPageHandler):
     return html
 
   def query_to_data(self, query):
-    query_template = """<div class="lt-query">
-      <h2>%(name)s</h2>
-      <table class='lt-data-table'>
-        <thead>
-          <td class='date-cell'>Date</td>
-          <td class='text-cell'>Response</td>
-          <td>Delete</td>
-        <thead>
-        %(rows)s
-        <tr class='input-row' id='input-row-%(query_id)s'>
-          <td class='date-cell'>Now</td>
-          <td class='text-cell'><input type='text' value='value'/></td>
-          <td><a class='new-entry-submit-button' id='new-entry-submit-button-%(query_id)s' href='#'>Submit</a></td>
-        </tr>
-      </table>
-      <p class='new-entry'><a class='new-entry-button' id='new-entry-button-%(query_id)s' href="#">New entry</a> <a class='import-data-button' href='import?query_id=%(query_id)s'>Import Data</a> <a class='export-data-button' href='export?query_id=%(query_id)s'>Export Data</a> <a href='analyze?query_id=%(query_id)s'>Analyze</a></p>
-    </div>"""
+    # keys
+    mck_metric_last_updated = str(query.key()) + '.last-updated'
+    mck_data_last_updated = str(query.key()) + '.data-last-updated'
+    mck_data = str(query.key()) + '.data'
+
+    # values
+    metric_last_updated = memcache.get(mck_metric_last_updated)
+    data_last_updated = memcache.get(mck_data_last_updated)
+    data = memcache.get(mck_data)
+ 
+    # cache miss condition
+    if not (data is not None and metric_last_updated is not None and data_last_updated is not None and int(metric_last_updated) < int(data_last_updated)):
+
+      # cache miss thingy
+      data = self.query_data_from_db(query)
+
+      # update the relevant keys
+      memcache.set(
+        key=mck_data,
+        value=data,
+        time=86400
+      )
+
+      memcache.set(
+        key=mck_data_last_updated,
+        value=datetime.now().strftime('%s'),
+        time=86400
+      )
+
+      if metric_last_updated is None:
+        memcache.set(
+          key=mck_metric_last_updated,
+          value=datetime.now().strftime('%s'),
+          time=86400
+        )
+
+    return data 
+
+  def query_data_from_db(self, query):
+    query_template = open('ui/html/metric_data.html').read()
 
     rows =  ''
     # get all datapoints associated with the query
@@ -61,8 +87,14 @@ class ManageDataHandler(LoggedInPageHandler):
     #   append data_point_to_row(dp)
     for dp in datapoints:
       rows += self.data_point_to_row(dp)
-   
-    return query_template % {'rows': rows, 'name': query.name, 'query_id': query.key()}
+  
+    params =  {
+      'rows': rows, 
+      'name': query.name, 
+      'query_id': query.key()
+    }
+
+    return query_template % params
 
   def data_point_to_row(self, dp):
     row_template = """<tr id='delete-row-%(dp_id)s'><td>%(date)s</td><td>%(text)s</td><td><a id='delete-%(dp_id)s' class='dp-delete-button' href='#'>Delete</a></td></tr>"""
