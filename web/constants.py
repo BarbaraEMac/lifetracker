37c530d3758ceb1db28ed3e1db1b6ff8d402b22b
gareth <gareth@PaloAlto.(none)>
2011-11-23 19:11:47 -0500

    A bunch more whitelisting.

--- web/constants.py
+++ web/constants.py
@@ -11,6 +11,10 @@ whitelist = [
   'shan.lian91@gmail.com',
   'robertdelder@gmail.com',
   'scott.tolksdorf@gmail.com',
+  'mr.rossrobinson@gmail.com',
+  'z4beth@gmail.com',
+  'dav.villa@gmail.com',
+  'the.hongwei@gmail.com',
  ]
 
 admins = [
